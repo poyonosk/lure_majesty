1c3a080325a16e2f312a2e9593cc403ac80cfc21
poyonosk <117244700+poyonosk@users.noreply.github.com>
2023-02-02 11:26:49 +0000

    [ADD]Gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  # ユーザー用sign_in
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  # 管理者用sign_in
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+  # ユーザー側のルーティング設定
+  root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
